ac87710bf79f0ac5453a66e262f2b81546471ed9
Mathieu Méa <mathieu.mea@gmail.com>
2019-06-14 07:29:38 -0400

    Compatibility with latest update

--- src/org/mtransit/parser/ca_whistler_transit_system_bus/WhistlerTransitSystemBusAgencyTools.java
+++ src/org/mtransit/parser/ca_whistler_transit_system_bus/WhistlerTransitSystemBusAgencyTools.java
@@ -508,6 +508,7 @@ public class WhistlerTransitSystemBusAgencyTools extends DefaultAgencyTools {
 		return CleanUtils.cleanLabel(tripHeadsign);
 	}
 
+	private static final Pattern STARTS_WITH_DCOM = Pattern.compile("(^(\\(\\-DCOM\\-\\)))", Pattern.CASE_INSENSITIVE);
 	private static final Pattern STARTS_WITH_IMPL = Pattern.compile("(^(\\(\\-IMPL\\-\\)))", Pattern.CASE_INSENSITIVE);
 	private static final Pattern STARTS_WITH_BOUND = Pattern.compile("(^(east|west|north|south)bound)", Pattern.CASE_INSENSITIVE);
 
@@ -516,6 +517,7 @@ public class WhistlerTransitSystemBusAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public String cleanStopName(String gStopName) {
+		gStopName = STARTS_WITH_DCOM.matcher(gStopName).replaceAll(StringUtils.EMPTY);
 		gStopName = STARTS_WITH_IMPL.matcher(gStopName).replaceAll(StringUtils.EMPTY);
 		gStopName = STARTS_WITH_BOUND.matcher(gStopName).replaceAll(StringUtils.EMPTY);
 		gStopName = AT.matcher(gStopName).replaceAll(AT_REPLACEMENT);
